51638c2a44e3f56a4fccb8c9aebc1b68507060c3
Francisco Vinicius <106833771+fviniciusb@users.noreply.github.com>
2023-11-14 16:12:02 -0400

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,19 @@
+// Funções Js
+
+// Definindo função
+
+function saudacao(nome,curso='javascript'){
+    console.log(`olá ${nome}, bem vindo ao curso de ${curso}`)
+}
+saudacao(nome)
+
+// como enviar parâmetros para a função
+
+
+function soma(n1,n2){
+    return n1+n2
+}
+
+const resulatado = soma(10,30)
+
+console.log(resulatado/2)
